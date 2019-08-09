4bf843816a3c123d0c7674de7198100b9f82f7f2
Juan Pardiñas Bárbara <jpardinas23@gmail.com>
2019-08-09 23:42:56 +0200

    now firebase obtain the data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,6 +39,48 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollections = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  /*We passin in our initial object the initial object goes into the first new collection.
+First element and it sets the first value equal to the title but in lowercase. So the first value is going to be hats.
+
+So it'll be an empty object with a property of hearts that's equal to the heart's collection. And then it returns that object.
+
+And it goes into the second object if the second object is say jackets then it's going to set a new property called jackets and then equal the jackets collection.
+
+So now you have an object that has a hats property equal to the hats collection and a jacket's property equal to the jackets collection and then so on and so forth until we end up with an object where the titles of all five of the collection objects are the keys and then they equal their respective collection object.*/
+
+  return transformedCollections.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
